013f42af2515ea80784e52e9a121c8c058371994
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2023-11-06 16:55:00 +0100

    feat: testing github actions

---
+++ src/app/components/gt-action-test-component.tsx
@@ -0,0 +1,11 @@
+import { Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+
+export function GithubActionTestComponents(): React.JSX.Element {
+  return (
+    <VStack marginY={"350px"}>
+      {/* <Text color={"white"}>GithubActionTestComponents</Text> */}
+    </VStack>
+  );
+}
