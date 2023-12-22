c12b502518312c811bdcadfc21f92318597268a5
Róza Nagy <rozanagy88@gmail.com>
2023-12-22 16:33:59 +0700

    fix: align components
    Co-authored-by: Polybius93 <Polybius93@users.noreply.github.com>

--- src/app/components/how-it-works/middle/components/explanation-block.tsx
+++ src/app/components/how-it-works/middle/components/explanation-block.tsx
@@ -1,28 +1,25 @@
-import { Box, Image, Text, VStack } from '@chakra-ui/react';
+import { Box, Stack, Text, VStack } from '@chakra-ui/react';
 
 interface ExplanationBlockProps {
-  src: string;
   title: string;
   content: React.ReactNode;
+  image: React.ReactNode;
 }
 
 export function ExplanationBlock({
-  src,
   title,
   content,
+  image,
 }: ExplanationBlockProps): React.JSX.Element {
   return (
     <VStack w={'316px'} alignItems={'left'}>
-      <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
-        <Image
-          src={src}
-          w={src === '/images/dlc-img.png' ? '315px' : '200px'}
-          h={src === '/images/dlc-img.png' ? '120px' : '100px'}
-        />
+      <Box display={'flex'} justifyContent={'center'} alignItems={'center'} h={'120px'}>
+        {image}
       </Box>
-
-      <Text variant={'title'}>{title}</Text>
-      {content}
+      <Stack h={'135px'}>
+        <Text variant={'title'}>{title}</Text>
+        {content}
+      </Stack>
     </VStack>
   );
 }
--- src/app/components/how-it-works/middle/middle-section.tsx
+++ src/app/components/how-it-works/middle/middle-section.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Text } from '@chakra-ui/react';
+import { Box, HStack, Image, Text } from '@chakra-ui/react';
 
 import { ExplanationBlock } from './components/explanation-block';
 
@@ -19,8 +19,8 @@ export function MiddleSection(): React.JSX.Element {
     >
       <HStack w={'1000px'} justifyContent={'space-between'}>
         <ExplanationBlock
-          src={'/images/mint-btc-grad.png'}
           title={'What is dlcBTC?'}
+          image={<Image src={'/images/mint-btc-grad.png'} w={'200px'} h={'100px'} ml={'89px'} />}
           content={
             <Text color={'white'}>
               dlcBTC is a{' '}
@@ -32,8 +32,8 @@ export function MiddleSection(): React.JSX.Element {
           }
         ></ExplanationBlock>
         <ExplanationBlock
-          src={'/images/dlc-img.png'}
           title={'How Does It Work?'}
+          image={<Image src={'/images/dlc-img.png'} w={'315px'} h={'120px'} />}
           content={
             <Text color={'white'}>
               <Text as={'span'} variant={'navigate'}>
@@ -44,8 +44,8 @@ export function MiddleSection(): React.JSX.Element {
           }
         ></ExplanationBlock>
         <ExplanationBlock
-          src={'/images/unmint-btc-grad.png'}
           title={'Why is It Safer?'}
+          image={<Image src={'/images/unmint-btc-grad.png'} w={'200px'} h={'100px'} mr={'89px'} />}
           content={
             <Text color={'white'}>
               Your Bitcoin remains secure on its native chain and cannot be redirected or stolen.
