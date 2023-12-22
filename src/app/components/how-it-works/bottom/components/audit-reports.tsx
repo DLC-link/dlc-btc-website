56f3931825b84ffa1a0c8ae67d3493527bb734f9
Róza Nagy <rozanagy88@gmail.com>
2023-12-22 11:05:14 +0700

    feat: change text and add link to audit report

--- src/app/components/how-it-works/bottom/components/audit-reports.tsx
+++ src/app/components/how-it-works/bottom/components/audit-reports.tsx
@@ -1,16 +1,24 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { HStack, Image, Link, Text } from '@chakra-ui/react';
 import { CustomCard } from '@components/how-it-works/components/custom-card';
 
 export function AuditReports(): React.JSX.Element {
   return (
-    <CustomCard width={'488px'} height={'152px'} padding={'25px'}>
+    <CustomCard width={'488px'} height={'170px'} padding={'25px'}>
       {
         <>
           <Text variant={'title'}>Audit Reports</Text>
-          <HStack>
-            <Image src={'/images/report.png'} w={'34px'} h={'42px'}></Image>
+          <HStack align={'start'} spacing={'15px'}>
+            <Image src={'/images/report.png'} w={'34px'} h={'42px'} pt={'5px'}></Image>
             <Text color={'white'}>
-              Coin Fabric: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.
+              The following is a security audit report, completed by the team at CoinFabrik on
+              November 29, 2023.{' '}
+              <Link
+                href={'https://docs.dlc.link/resources#security-audit-reports'}
+                isExternal
+                color={'accent.cyan.01'}
+              >
+                Read the report...
+              </Link>
             </Text>
           </HStack>
         </>
