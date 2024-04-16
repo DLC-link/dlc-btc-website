557d1a113b4ddf0965579287a3c24a3ffcd1bba5
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2024-04-16 16:27:36 +0400

    feat: replace halfhourfee with hour fee

--- src/app/hooks/use-bitcoin.ts
+++ src/app/hooks/use-bitcoin.ts
@@ -157,7 +157,7 @@ export function useBitcoin(): UseBitcoinReturnType {
       const response = await fetch(mempoolSpaceAPIFeeURL);
       const feeRates: FeeRates = await response.json();
 
-      return feeRates.halfHourFee;
+      return feeRates.fastestFee;
     } catch (error) {
       throw new BitcoinError(`Error getting Fee Rate: ${error}`);
     }
