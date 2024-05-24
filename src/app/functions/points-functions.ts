682e8df42307acc4dccede66ca22f0ee2ca280a8
sosaucily <sosaucily@gmail.com>
2024-05-24 11:18:08 +0200

    feat: add comments

--- src/app/functions/points-functions.ts
+++ src/app/functions/points-functions.ts
@@ -26,30 +26,43 @@ async function fetchTransferEvents() {
   return detailedEvents;
 }
 
+// My vision is that the pointsMap which is being used in the function to store the points per time would look like this:
+// {
+//   [ // list of addresses
+//     {
+//       '0x123':
+//       [
+//         { timestamp: 1650000000, amount: 50}, // at time 1650000000 Points are 100 minted - 50 burned
+//         { tiemstamp: 1700000000, amount: -20}, // at time 1700000000 Points are 20 burned
+//       ],
+//     }
+//   ]
+// }
+
 // Function to calculate points for tokens minted over time, grouped by hourly slots
 export function calculateMintedPointsHourly(events) {
-  const pointMap = {};
+  const pointsMap = {};
 
   events.forEach(({ from, to, amount, timestamp }) => {
     const hourSlot = Math.floor(timestamp / 3600) * 3600; // Timestamp rounded down to the nearest hour
 
-    if (!(to in pointMap)) {
-      pointMap[to] = {};
+    if (!(to in pointsMap)) {
+      pointsMap[to] = {};
     }
 
-    if (!(hourSlot in pointMap[to])) {
-      pointMap[to][hourSlot] = 0;
+    if (!(hourSlot in pointsMap[to])) {
+      pointsMap[to][hourSlot] = 0;
     }
 
     if (from === burnAddress) {
-      pointMap[to][hourSlot] += amount.toNumber();
+      pointsMap[to][hourSlot] += amount.toNumber();
     } else if (to === burnAddress) {
-      pointMap[from][hourSlot] = (pointMap[from][hourSlot] || 0) - amount.toNumber();
+      pointsMap[from][hourSlot] = (pointsMap[from][hourSlot] || 0) - amount.toNumber();
     }
   });
 
   // Output the results
-  for (const [address, hours] of Object.entries(pointMap)) {
+  for (const [address, hours] of Object.entries(pointsMap)) {
     console.log(`Address: ${address}`);
     for (const [hour, points] of Object.entries(hours)) {
       console.log(`Hour: ${new Date(hour * 1000).toLocaleString()}, Points: ${points}`);
