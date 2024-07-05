cc9ec7b09b063a296f00e512965e164efdac223b
scolear <bobtera@gmail.com>
2024-07-05 17:01:15 +0200

    refactor: typescript netlify functions

--- netlify/functions/register-user-address.ts
+++ netlify/functions/register-user-address.ts
@@ -1,6 +1,26 @@
-export async function handler(event) {
+import { Handler } from '@netlify/functions';
+
+const handler: Handler = async event => {
   const chainalysisURL = process.env.VITE_CHAINALYSIS_API_URL;
   const chainalysisToken = process.env.VITE_CHAINALYSIS_TOKEN;
+
+  if (!chainalysisURL || !chainalysisToken) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Chainalysis API URL and token are required',
+      }),
+    };
+  }
+
+  if (!event.queryStringParameters) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Address is required',
+      }),
+    };
+  }
 
   const options = {
     method: 'POST',
@@ -30,4 +50,6 @@ export async function handler(event) {
       message: await response.json(),
     }),
   };
-}
+};
+
+export { handler };
