527e30cf4bb40bd1a44859c6b40e33ac8352f0c6
Róza Nagy <rozanagy88@gmail.com>
2024-07-08 15:04:44 +0200

    fix: delete utilities file

--- src/shared/utilities.ts
+++
@@ -1,6 +0,0 @@
-export const formatName = (name: string): string => {
-  return name
-    .split(' ')
-    .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(' ');
-};
