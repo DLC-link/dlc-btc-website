e10fc571f040357917d8a947bbb130e980ef5546
Roza Eisenberg <45665959+rozanagy@users.noreply.github.com>
2024-07-10 15:22:46 +0200

    Feat/points table (#128)
    * feat: add table to points page

---
+++ src/app/components/points/components/points-table/components/points-table-item.tsx
@@ -0,0 +1,49 @@
+import { HStack, Image, Text } from '@chakra-ui/react';
+import { CustomSkeleton } from '@components/custom-skeleton/custom-skeleton';
+import { ProtocolRewards } from '@models/ethereum-models';
+import { unshiftValue } from 'dlc-btc-lib/utilities';
+
+import { formatNumber } from '@shared/utils';
+
+export function PointsTableItem(pointsTableItem: ProtocolRewards): React.JSX.Element {
+  if (!pointsTableItem) return <CustomSkeleton height={'35px'} />;
+
+  const { name, points, currentDLCBTC, multiplier } = pointsTableItem;
+
+  return (
+    <HStack
+      p={'10px'}
+      w={'100%'}
+      h={'50px'}
+      bg={'background.container.01'}
+      blendMode={'screen'}
+      border={'1px solid'}
+      borderRadius={'md'}
+      borderColor={'border.white.01'}
+      justifyContent={'space-between'}
+    >
+      <HStack w={'25%'}>
+        <Image src={'/images/logos/dlc-btc-logo.svg'} alt={'dlc BTC logo'} boxSize={'25px'} />
+        <Text color={'white'} fontSize={'sm'} fontWeight={800}>
+          {unshiftValue(currentDLCBTC)}
+        </Text>
+      </HStack>
+      <HStack w={'50%'}>
+        <Text color={'white'} fontSize={'sm'} fontWeight={800}>
+          {`${formatNumber(points)} Points`}
+        </Text>
+        <Text
+          bgGradient={`linear(to-r, #AC50EF, #7059FB, #2ECFF6)`}
+          bgClip="text"
+          fontSize={'sm'}
+          fontWeight={800}
+        >
+          {`(${multiplier}x)`}
+        </Text>
+      </HStack>
+      <Text w={'20%'} color={'white'} fontSize={'sm'}>
+        {name}
+      </Text>
+    </HStack>
+  );
+}
