21e495ffaa8e835f97fe366f16cf3e2911d80b92
Róza Nagy <rozanagy88@gmail.com>
2024-07-19 10:47:03 +0200

    fix: lint prettier

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,15 +1,15 @@
 import Decimal from 'decimal.js';
 
 export function formatNumber(value: number): string {
-    if (value < 10000) {
-        return value.toString();
-    } else if (value < 1000000) {
-        return new Decimal(value).dividedBy(1000).toFixed(1).replace(/\.0$/, '') + 'K';
-    } else if (value < 1000000000) {
-        return new Decimal(value).dividedBy(1000000).toFixed(1).replace(/\.0$/, '') + 'M';
-    } else if (value < 1000000000000) {
-        return new Decimal(value).dividedBy(1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
-    } else {
-        return new Decimal(value).dividedBy(1000000000000).toFixed(1).replace(/\.0$/, '') + 'T';
-    }
+  if (value < 10000) {
+    return value.toString();
+  } else if (value < 1000000) {
+    return new Decimal(value).dividedBy(1000).toFixed(1).replace(/\.0$/, '') + 'K';
+  } else if (value < 1000000000) {
+    return new Decimal(value).dividedBy(1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+  } else if (value < 1000000000000) {
+    return new Decimal(value).dividedBy(1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
+  } else {
+    return new Decimal(value).dividedBy(1000000000000).toFixed(1).replace(/\.0$/, '') + 'T';
+  }
 }
