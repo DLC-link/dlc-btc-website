dc6c530ac146da709fac2fce071857936e2fa3d8
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2024-07-23 10:36:46 +0200

    feat: remove null check from getEthereumNetworkDeploymentPlans function

--- src/app/functions/configuration.functions.ts
+++ src/app/functions/configuration.functions.ts
@@ -1,17 +1,12 @@
 import { getEthereumContract, getProvider } from 'dlc-btc-lib/ethereum-functions';
 import { EthereumDeploymentPlan, EthereumNetwork } from 'dlc-btc-lib/models';
 import { Contract } from 'ethers';
-import { isNil } from 'ramda';
 
 export function getEthereumNetworkDeploymentPlans(
   ethereumNetwork: EthereumNetwork
 ): EthereumDeploymentPlan[] {
   const ethereumNetworkDeploymentPlans: EthereumDeploymentPlan[] =
     appConfiguration.ethereumContractInformations[ethereumNetwork.name.toLowerCase()];
-
-  if (isNil(ethereumNetworkDeploymentPlans)) {
-    throw new Error('Deployment plans not found');
-  }
 
   return ethereumNetworkDeploymentPlans;
 }
