6a1ad461875993f6d0bcc265c1b9dde5b4f1b61f
Róza Nagy <rozanagy88@gmail.com>
2024-07-31 13:24:29 +0200

    feat: add modal for jasper vault

---
+++ src/app/components/modals/jasper-modal/jasper-modal.tsx
@@ -0,0 +1,21 @@
+import { Button, Image, Text, VStack } from '@chakra-ui/react';
+import { ModalComponentProps } from '@components/modals/components/modal-container';
+import { ModalLayout } from '@components/modals/components/modal.layout';
+
+export function JasperModal({ isOpen, handleClose }: ModalComponentProps): React.JSX.Element {
+  return (
+    <ModalLayout title="" isOpen={isOpen} onClose={() => handleClose()}>
+      <VStack alignItems={'start'} spacing={'25px'}>
+        <Image src={'/images/logos/jasper-vault-logo.svg'} alt={'Jasper Vault Logo'} />
+        <Text color={'accent.lightBlue.01'} fontSize={'xl'} fontWeight={800}>
+          Jasper Vault Points Update
+        </Text>
+        <Text color={'white.02'}>
+          Your Jasper Vault points are safe and will appear soon. We’ll ensure everything is updated
+          retroactively. Thank you for your patience!
+        </Text>
+        <Button variant={'account'}>Continue to Points</Button>
+      </VStack>
+    </ModalLayout>
+  );
+}
