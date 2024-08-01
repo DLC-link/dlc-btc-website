229d241e3ac45dac67be16a6f326eabad446ecfa
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2024-08-01 13:42:23 +0200

    feat: modify select wallet modal to handle visually pending and success states

--- src/app/components/modals/select-wallet-modal/components/select-wallet-menu.tsx
+++ src/app/components/modals/select-wallet-modal/components/select-wallet-menu.tsx
@@ -1,23 +1,46 @@
-import { Button, HStack, Image, Text } from '@chakra-ui/react';
+import { Box, Button, HStack, Image, Spinner, Text } from '@chakra-ui/react';
 import { Connector } from 'wagmi';
 
 interface SelectWalletMenuProps {
   wagmiConnector: Connector;
+  selectedWagmiConnectorID?: string;
+  isConnectWalletPending: boolean;
+  isConnectWalletSuccess: boolean;
   handleConnectWallet: (wagmiConnector: Connector) => void;
 }
 
 export function SelectWalletMenu({
   wagmiConnector,
+  selectedWagmiConnectorID,
+  isConnectWalletPending,
+  isConnectWalletSuccess,
   handleConnectWallet,
 }: SelectWalletMenuProps): React.JSX.Element {
-  const { icon, name } = wagmiConnector;
+  const { id, icon, name } = wagmiConnector;
+
+  const isThisWalletSelected = selectedWagmiConnectorID === id;
 
   return (
-    <Button variant={'wallet'} onClick={() => handleConnectWallet(wagmiConnector)}>
-      <HStack justifyContent={'space-evenly'} w={'250px'}>
-        <Image src={icon} alt={name} boxSize={'25px'} />
-        <Text w={'150px'}>{name}</Text>
-      </HStack>
+    <Button
+      borderColor={
+        isConnectWalletSuccess && isThisWalletSelected ? 'accent.lightBlue.01' : 'border.white.01'
+      }
+      variant={'wallet'}
+      onClick={() => handleConnectWallet(wagmiConnector)}
+    >
+      <Box position="relative" w={'100%'} display={'flex'} justifyContent={'center'}>
+        <HStack
+          justifyContent={'space-evenly'}
+          w={'250px'}
+          filter={isConnectWalletPending && isThisWalletSelected ? 'opacity(25%)' : 'none'}
+        >
+          <Image src={icon} alt={name} boxSize={'25px'} />
+          <Text w={'150px'}>{name}</Text>
+        </HStack>
+        {isConnectWalletPending && isThisWalletSelected && (
+          <Spinner size={'md'} color={'accent.lightBlue.01'} position={'absolute'} />
+        )}
+      </Box>
     </Button>
   );
 }
--- src/app/components/modals/select-wallet-modal/select-wallet-modal.tsx
+++ src/app/components/modals/select-wallet-modal/select-wallet-modal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { CheckIcon } from '@chakra-ui/icons';
 import { HStack, ScaleFade, Text, VStack } from '@chakra-ui/react';
@@ -6,18 +6,37 @@ import { ModalComponentProps } from '@components/modals/components/modal-contain
 import { ModalLayout } from '@components/modals/components/modal.layout';
 import { SelectWalletMenu } from '@components/modals/select-wallet-modal/components/select-wallet-menu';
 import { SelectNetworkButton } from '@components/select-network-button/select-network-button';
+import { delay } from 'dlc-btc-lib/utilities';
 import { Chain } from 'viem';
 import { Connector, useConfig, useConnect } from 'wagmi';
 
 export function SelectWalletModal({ isOpen, handleClose }: ModalComponentProps): React.JSX.Element {
-  const { connectors, connect } = useConnect();
+  const { connectors, connect, isPending, isSuccess } = useConnect();
   const { chains } = useConfig();
 
   const [selectedEthereumNetwork, setSelectedEthereumNetwork] = useState<Chain | undefined>(
     undefined
   );
+  const [selectedWagmiConnectorID, setSelectedWagmiConnectorID] = useState<string | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (isSuccess) {
+      void handleCloseAfterSuccess();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSuccess]);
+
+  async function handleCloseAfterSuccess() {
+    await delay(2500);
+    setSelectedEthereumNetwork(undefined);
+    setSelectedWagmiConnectorID(undefined);
+    handleClose();
+  }
 
   async function handleConnectWallet(wagmiConnector: Connector) {
+    setSelectedWagmiConnectorID(wagmiConnector.id);
     connect({ chainId: selectedEthereumNetwork?.id, connector: wagmiConnector });
   }
 
@@ -31,7 +50,7 @@ export function SelectWalletModal({ isOpen, handleClose }: ModalComponentProps):
         {!selectedEthereumNetwork ? (
           <Text variant={'header'}>Select Network</Text>
         ) : (
-          <HStack>
+          <HStack w={'100%'} justifyContent={'space-between'}>
             <Text variant={'header'}>Network Selected</Text>
             <CheckIcon color={'accent.lightBlue.01'} />
           </HStack>
@@ -47,11 +66,21 @@ export function SelectWalletModal({ isOpen, handleClose }: ModalComponentProps):
           unmountOnExit
         >
           <VStack alignItems={'start'} spacing={'25px'}>
-            <Text variant={'header'}>Select Wallet</Text>
+            {!isSuccess ? (
+              <Text variant={'header'}>Select Wallet</Text>
+            ) : (
+              <HStack w={'100%'} justifyContent={'space-between'}>
+                <Text variant={'header'}>Wallet Connected</Text>
+                <CheckIcon color={'accent.lightBlue.01'} />
+              </HStack>
+            )}
             {connectors.map(wagmiConnector => (
               <SelectWalletMenu
                 key={wagmiConnector.id}
                 wagmiConnector={wagmiConnector}
+                selectedWagmiConnectorID={selectedWagmiConnectorID}
+                isConnectWalletPending={isPending}
+                isConnectWalletSuccess={isSuccess}
                 handleConnectWallet={handleConnectWallet}
               />
             ))}
