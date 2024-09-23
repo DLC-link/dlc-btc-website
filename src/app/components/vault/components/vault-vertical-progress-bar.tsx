e0b008eb924c544fa7c403bba91b763ea29e2f47
Róza Nagy <rozanagy88@gmail.com>
2024-09-23 10:29:56 +0200

    feat: create statusindicator component

--- src/app/components/vault/components/vault-vertical-progress-bar.tsx
+++ src/app/components/vault/components/vault-vertical-progress-bar.tsx
@@ -1,26 +1,42 @@
 import { Divider, Image, Spinner, VStack } from '@chakra-ui/react';
 
-interface VaultVerticalProgressBarProps {
-  stateA: boolean;
-  stateB: boolean;
+export enum Status {
+  Completed = 'completed',
+  Current = 'current',
+  Inactive = 'inactive',
 }
 
-const StatusIndicator: React.FC<{ state: boolean }> = ({ state }) =>
-  state ? (
-    <Image src={'/images/check.svg'} alt={'Icon'} boxSize={'20px'} />
-  ) : (
-    <Spinner size={'md'} color={'accent.lightBlue.01'} />
-  );
+interface VaultVerticalProgressBarProps {
+  statusA: Status;
+  statusB: Status;
+}
+
+const StatusIndicator = {
+  status: Status.Inactive,
+
+  getComponent: (status: Status) => {
+    switch (status) {
+      case Status.Completed:
+        return <Image src={'/images/check.svg'} alt={'Icon'} boxSize={'20px'} />;
+      case Status.Current:
+        return <Spinner size={'md'} color={'accent.lightBlue.01'} />;
+      case Status.Inactive:
+        return <Image src={'/images/loader.svg'} alt={'Inactive Loader'} boxSize={'20px'} />; // Customize spinner as needed
+      default:
+        return null;
+    }
+  },
+};
 
 export function VaultVerticalProgressBar({
-  stateA,
-  stateB,
+  statusA,
+  statusB,
 }: VaultVerticalProgressBarProps): React.JSX.Element {
   return (
     <VStack w={'100%'} alignItems={'center'}>
-      <StatusIndicator state={stateA} />
-      <Divider orientation={'vertical'} height={'250px'} variant={'thick'} />
-      <StatusIndicator state={stateB} />
+      {StatusIndicator.getComponent(statusA)}
+      <Divider orientation={'vertical'} height={'150px'} variant={'thick'} />
+      {StatusIndicator.getComponent(statusB)}
     </VStack>
   );
 }
