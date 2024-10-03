e0e1b3d9e59ff39657d7d2213e4de9acf5f39ce7
Róza Nagy <rozanagy88@gmail.com>
2024-10-03 15:29:56 +0200

    fix: update points table content alignment

--- src/app/components/points/components/points-table/components/points-table-item.tsx
+++ src/app/components/points/components/points-table/components/points-table-item.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { HStack, Image, Text, useBreakpointValue } from '@chakra-ui/react';
 import { CustomSkeleton } from '@components/custom-skeleton/custom-skeleton';
 import { ProtocolRewards } from '@models/points.models';
 import { unshiftValue } from 'dlc-btc-lib/utilities';
@@ -6,6 +6,7 @@ import { unshiftValue } from 'dlc-btc-lib/utilities';
 import { formatNumber } from '@shared/utils';
 
 export function PointsTableItem(pointsTableItem: ProtocolRewards): React.JSX.Element {
+  const isMobile = useBreakpointValue({ base: true, md: false });
   if (!pointsTableItem) return <CustomSkeleton height={'35px'} />;
 
   const { name, points, currentTokens, multiplier } = pointsTableItem;
@@ -22,30 +23,55 @@ export function PointsTableItem(pointsTableItem: ProtocolRewards): React.JSX.Ele
       borderColor={'border.white.01'}
       justifyContent={'space-between'}
     >
-      <HStack w={'25%'}>
-        <Image src={'/images/logos/dlc-btc-logo.svg'} alt={'dlc BTC logo'} boxSize={'25px'} />
-        <Text color={'white'} fontSize={'sm'} fontWeight={800}>
-          {unshiftValue(currentTokens)}
-        </Text>
-      </HStack>
-      <HStack w={'50%'}>
-        <Text color={'white'} fontSize={'sm'} fontWeight={800}>
-          {`${formatNumber(points)} Points`}
-        </Text>
-        <Text
-          bgGradient={`linear(to-r, #AC50EF, #7059FB, #2ECFF6)`}
-          bgClip="text"
-          fontSize={'sm'}
-          fontWeight={800}
-        >
-          {`(${multiplier}x)`}
-        </Text>
-      </HStack>
-      <HStack w={'25%'}>
-        <Text color={'white'} fontSize={'sm'}>
-          {name}
-        </Text>
-      </HStack>
+      {isMobile ? (
+        <HStack w={'100%'}>
+          <HStack w={'100%'}>
+            <Text color={'white'} fontSize={'sm'}>
+              {name}
+            </Text>
+          </HStack>
+          <HStack w={'50%'}>
+            <Text color={'white'} fontSize={'sm'} fontWeight={800}>
+              {formatNumber(points)}
+            </Text>
+            <Text
+              bgGradient={`linear(to-r, #AC50EF, #7059FB, #2ECFF6)`}
+              bgClip="text"
+              fontSize={'sm'}
+              fontWeight={800}
+            >
+              {`(${multiplier}x)`}
+            </Text>
+          </HStack>
+        </HStack>
+      ) : (
+        <>
+          <HStack w={'25%'}>
+            <Image src={'/images/logos/dlc-btc-logo.svg'} alt={'dlc BTC logo'} boxSize={'25px'} />
+            <Text color={'white'} fontSize={'sm'} fontWeight={800}>
+              {unshiftValue(currentTokens)}
+            </Text>
+          </HStack>
+          <HStack w={'50%'}>
+            <Text color={'white'} fontSize={'sm'} fontWeight={800}>
+              {`${formatNumber(points)} Points`}
+            </Text>
+            <Text
+              bgGradient={`linear(to-r, #AC50EF, #7059FB, #2ECFF6)`}
+              bgClip="text"
+              fontSize={'sm'}
+              fontWeight={800}
+            >
+              {`(${multiplier}x)`}
+            </Text>
+          </HStack>
+          <HStack w={'25%'}>
+            <Text color={'white'} fontSize={'sm'}>
+              {name}
+            </Text>
+          </HStack>
+        </>
+      )}
     </HStack>
   );
 }
