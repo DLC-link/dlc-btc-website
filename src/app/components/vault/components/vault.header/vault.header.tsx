fdf2de2d49ef56960362857b92102871f94a181f
Róza Nagy <rozanagy88@gmail.com>
2024-10-07 10:59:24 +0200

    feat: remove text and adjust copy button

--- src/app/components/vault/components/vault.header/vault.header.tsx
+++ src/app/components/vault/components/vault.header/vault.header.tsx
@@ -19,26 +19,15 @@ export function VaultHeader({
 
   return (
     <HStack w={'100%'} justifyContent={'space-between'}>
-      <HStack
-        w={'67.5%'}
-        pl={'15px'}
-        bg={'white.04'}
-        borderRadius={'full'}
-        justifyContent={'space-between'}
-      >
-        <Stack w={'25%'}>
-          <Text fontSize={'xs'} fontWeight={'bold'} color={'white.01'}>
-            Vault
-          </Text>
-        </Stack>
-        <Stack w={'50%'}>
+      <HStack pl={'10px'} bg={'white.04'} borderRadius={'full'}>
+        <Stack>
           <Text fontSize={'xs'} fontWeight={'bold'} color={'white.01'}>
             {vaultTruncatedUUID}
           </Text>
         </Stack>
         <VaultCopyButton onCopyUUID={onCopy} hasCopiedUUID={hasCopied} />
       </HStack>
-      <HStack w={'50%'} justifyContent={'flex-end'}>
+      <HStack justifyContent={'flex-end'}>
         <Text fontSize={'xs'} color={'grey.01'}>
           {vaultCreationDate}
         </Text>
